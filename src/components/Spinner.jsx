d893cc96b8701ba83016ffeb78cf5975fe65f6e9
Osama Suleiman <osama.suleiman97@gmail.com>
2024-07-10 18:06:53 +0900

    Add spinner for loading

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import ClipLoader from "react-spinners/ClipLoader";
 
-const Spinner = () => {
+const override = {
+  display: 'block',
+  margin: '100px auto'
+}
+
+const Spinner = ({ loading }) => {
   return (
-    <div>
-      Spinner
-    </div>
+    <ClipLoader
+      color='#4338ca'
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
   )
 }
 
